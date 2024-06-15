a958b5318fd9adf9f19915c7b7dd43aaaf7e8f93
Junaid-Adil <167700525+Junaid-Adil@users.noreply.github.com>
2024-06-15 20:26:00 +0530

    added 3 python files

---
+++ prg1.py
@@ -0,0 +1,13 @@
+'''# print('Hello world!!!')
+def display_person(*args):
+    for i in args:
+        print(i)
+
+display_person(name="Emma", age="25")'''
+
+# Take the input from user
+
+iprompt = "Enter your Name: "
+first_name = input(iprompt)
+print(first_name)
+print(type(first_name))
---
+++ prg2.py
@@ -0,0 +1,7 @@
+#for function practise
+
+'''for  i in range(5,50,5):
+    print(i)'''
+
+for  value in range(100,50,-5):
+    print(value)
